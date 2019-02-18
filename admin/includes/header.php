9de521639ce8dccfee640b12a6a9dbd551778c6d
digdef <bydigdef@gmail.com>
2019-02-18 09:12:43 +0200

    blog

---
+++ admin/includes/header.php
@@ -0,0 +1,12 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css" id="changeable_styles"></link>
+</head>
+<header style="z-index: 999">	
+	<div id="logo" onclick="slowScroll('#top')">
+		<span>digDef</span>
+	</div>
+	<div id="about">
+		<a href="add.php">Добавить</a>
+		<a href="delete.php">Удалить</a>
+	</div>
+</header>
